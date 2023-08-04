2419ad0582cd7f0fe302b89b1a2e4ee93779b5f2
impactnelson <nghonelson14@gmail.com>
2023-08-04 05:23:34 -0700

    main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+unsigned int binary_to_unit(const char *j);
+void printing_binary(unsigned long int l);
+int geting_bits(unsigned long int l, unsigned int index);
+int setting_bits(unsigned long *l, unsigned int index);
+int clears_bits(unsigned long int *l, unsigned int index);
+unsigned int flips_bits(unsigned long int l, unsigned long int n);
+int _atoi(const char *n);
+int _putchar(char m);
+int gets_endianness(void);
+#endif
+/**
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
@@ -11,5 +21,4 @@ int _atoi(const char *s);
 int _putchar(char c);
 int get_endianness(void);
 
-#endif
-
+#endif*/
