5b95ceea966485e42ba051ca7e4117d1f13db622
impactnelson <nghonelson14@gmail.com>
2023-08-11 10:22:23 -0700

    updates

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,7 +9,7 @@
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
-	unsigned int dec_val = 0;
+	unsigned int value = 0;
 
 	if (!b)
 		return (0);
@@ -18,9 +18,9 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		dec_val = 2 * dec_val + (b[i] - '0');
+		value = 2 * value + (b[i] - '0');
 	}
 
-	return (dec_val);
+	return (value);
 }
 
