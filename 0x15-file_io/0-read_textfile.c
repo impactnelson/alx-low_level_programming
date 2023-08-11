d35a0811fc31cff8770a73acf37f693eb4135d95
impactnelson <nghonelson14@gmail.com>
2023-08-11 04:34:33 -0700

    updates

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,22 +1,34 @@
-#include <stdio.h>
+
 #include "main.h"
 
+#include <stdlib.h>
+
+
+
 /**
- * main - reads a file to standar output
- * return - returns (0): on success
+ * read_textfile- To read text form a file and print to standard ouput(STDOUT).
+ * @filename: this is the text file we are reading
+ * @letters: this is the number of letters we are going to be reading.
+ *
+ * Return: return n as the number of bytes read and displayed
+ *        return 0 when a function fails or when the filename is NULL.
  */
-
-int main(void)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
+	char *buffered;
+	ssize_t df;
+	ssize_t n;
+	ssize_t fd;
 
-	FILE *nels = fopen("nelson.txt", "r");
+	df = open(filename, O_RDONLY);
+	if (df == -1)
+		return (0);
+	buffered = malloc(sizeof(char) * letters);
+	fd = read(df, buffered, letters);
+	n = write(STDOUT_FILENO, buffered, fd);
 
-	fprintf(nels, "%s", "Hello everyone\n");
-
-	fclose(nels);
-
-	return (0);
-
-
-
+	free(buffered);
+	close(df);
+	return (n);
 }
+
