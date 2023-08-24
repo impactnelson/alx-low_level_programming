e250264f256c221e1b18236f0fc5dacf9b6b5c11
impactnelson <nghonelson14@gmail.com>
2023-08-24 16:53:30 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 
 /**
- * main - This is a A c program that prints a lin with std error.
+ * main - This c program prints a line of quote.
  * Return: Returns 1 for success, Otherwise.
  */
 int main(void)
 {
 	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	
+
 	return (1);
 }
