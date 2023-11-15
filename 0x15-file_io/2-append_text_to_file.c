1d7ad7fd5f9522e256093dc4baa383749ec2a7b4
impactnelson <nghonelson14@gmail.com>
2023-11-15 20:16:12 +0000

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 /**
  * append_text_to_file - Adds text at the file's tail.
  * @filename: Pointer to the file's name
@@ -12,23 +13,24 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int m, x, nel = 0;
-	if(filename==NULL)
-		return(-1);
-	if(text_content !=NULL)
-	{
-		for(nel=0; text_content[nel];)
-			nel++;
+        int m, x, nel = 0;
 
-	}
+        if (filename == NULL)
+                return (-1);
+        if (text_content != NULL)
+        {
+                for (nel = 0; text_content[nel];)
+                        nel++;
 
-	m = open(filename, O_WRONLY | O_APPEND);
-	x = write(m, text_content, nel);
-	
-	if(m==-1 || x==-1)
-		return(-1);
+        }
 
-	close(m);
-	return(1);
+        m = open(filename, O_WRONLY | O_APPEND);
+        x = write(m, text_content, nel);
+
+        if (m == -1 || x == -1)
+                return (-1);
+
+        close(m);
+        return (1);
 
 }
