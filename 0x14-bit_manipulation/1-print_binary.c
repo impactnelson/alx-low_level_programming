c142654449b4077dac6a2471512ebbd669408433
impactnelson <nghonelson14@gmail.com>
2023-11-16 15:40:25 +0000

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_binary - This will prints the binary equal to a decimal num
+ * @n: The num to be printed in bi.
+ */
+void print_binary(unsigned long int n)
+{
+	int j, count = 0;
+	unsigned long int current;
+
+	for (j = 63; j >= 0; j--)
+	{
+		current = n >> j;
+
+		if (current & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if (!count)
+		_putchar('0');
+}
+
